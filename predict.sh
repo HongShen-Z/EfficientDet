824e7339042715fd5699a58dce058151b5a6fd65
SEU <com.zhs@gmail.com>
2022-07-03 18:35:13 +0800

    test

---
+++ predict.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#BSUB -J predict
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -q gpu_v100
+#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
+
+module load anaconda3
+source activate
+conda deactivate
+conda activate torch1.2
+python predict.py 
+
+#--gpus $CUDA_VISIBLE_DEVICES
+#~/.cache/torch/checkpoints/yolov5s.pt
